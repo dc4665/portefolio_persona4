1932fdc6b0986932e9c410d7d09cc7131e35ba0b
dc4665 <daihiep.cao@gmail.com>
2021-05-24 11:23:14 +0200

    feature completed

--- src/components/SkillsLogo.js
+++ src/components/SkillsLogo.js
@@ -33,10 +33,39 @@ function SkillsLogo() {
         }
     }
 
+    function renderSkillsDescription(currentSkill) {
+        switch(currentSkill.currentSkill){
+            case "HTML - CSS3 - SASS":
+                return "HTML/CSS is pretty cool ! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis officia qui beatae ad optio cumque ducimus expedita neque temporibus voluptates?"
+            
+            case "Javascript - React":
+                return "Javascript is pretty cool ! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis officia qui beatae ad optio cumque ducimus expedita neque temporibus voluptates?"
+
+            case "MongoDB - Express - Node":
+                return "MongoDB is pretty cool ! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis officia qui beatae ad optio cumque ducimus expedita neque temporibus voluptates?"
+
+            case "PHP - MySQL - Wordpress":
+                return "PHP is pretty cool ! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis officia qui beatae ad optio cumque ducimus expedita neque temporibus voluptates?"
+
+            case "Photoshop - Figma":
+                return "Figma is pretty cool ! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis officia qui beatae ad optio cumque ducimus expedita neque temporibus voluptates?"
+
+            default:
+                return <p>Default Skill</p>    
+        }
+    }
+
     return (
-        <div className="skills-container">
-            {renderSkills(currentSkill)}
-        </div>
+        <>
+            <div className="skills-container">
+                {renderSkills(currentSkill)}
+            </div>
+
+            <div className="skills-description-container">
+                <h2>Info</h2>
+                <p>{renderSkillsDescription(currentSkill)}</p>
+            </div>
+        </>
     )
 }
 
